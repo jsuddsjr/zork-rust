2a18185a24ad8239d07c0c0ad90c34581c701396
John Sudds <jsuddsjr@users.noreply.github.com>
2023-11-01 19:04:27 -0700

    Dump the trash

--- dump/player.rs
+++
@@ -1,7 +0,0 @@
-struct Player {
-    name: String,
-    health: u8,
-    level: u8,
-    score: u32,
-    inventory: Inventory,
-}
--- dump/rooms/exit.rs
+++
@@ -1,6 +0,0 @@
-pub struct Exit {
-    scene: &Scene,
-    direction: Direction,
-    flags: Vec<String>,
-    // props: HashMap<String, V>,
-}
